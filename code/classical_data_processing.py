a565e2bec5ca9e8d6b4cfd1a8e298ed85f2c29fc
Tyler <txj3401@glados.cs.rit.edu>
2025-12-04 14:02:37 -0500

    Fixed and fitted models for K-Nearest Neighbors, Random Forest, AdaBoost, and LinearSVC.

--- code/classical_data_processing.py
+++ code/classical_data_processing.py
@@ -12,29 +12,35 @@ target_training_file = "../data/clean/twitter_train_balanced.csv"
 target_validation_file = "../data/clean/twitter_validation_clean.csv"
 target_test_file = "../data/clean/twitter_test_clean.csv"
 
-def process_file(fname):
-    if not os.path.exists(fname):
-        raise FileNotFoundError("Clean data files not found. Run data_processing.py and balance_data.py first.")
-    data_set = pd.read_csv(fname)
-    # Fitting tf-idf vectorizer
-    tfidf_vec = skl_text.TfidfVectorizer(min_df=0.002, max_df=0.6)
-    tfidf_matrix = tfidf_vec.fit_transform(data_set["Tweet Content"])
-
-    entenc = skl_pre.LabelBinarizer()
-    entity = entenc.fit_transform(data_set['Entity'].to_xarray())
-    sentenc = skl_pre.LabelEncoder()
-    sentiment = sentenc.fit_transform(data_set['Sentiment'])
-
-    class_names = sentenc.inverse_transform(range(4))
-    pd.DataFrame(class_names).to_csv("../data/class_names.csv")
-
-    X, y = np.concatenate((entity,tfidf_matrix.toarray()), axis=1), sentiment
+def vectorize(data_set, entenc, sentenc, tfidf_vec):
+    entity = entenc.transform(data_set["Entity"].to_xarray())
+    sentiment = sentenc.transform(data_set["Sentiment"])
+    tfidf_matrix = tfidf_vec.transform(data_set["Tweet Content"])
+    X, y = np.concatenate((entity, tfidf_matrix.toarray()), axis=1), sentiment
 
     return X, y
 
 def get_vectorized_data():
-    X_train, y_train = process_file(target_training_file)
-    X_val, y_val = process_file(target_validation_file)
-    X_test, y_test = process_file(target_test_file)
+    if not os.path.exists(target_training_file):
+        raise FileNotFoundError("Clean data files not found. Run data_processing.py and balance_data.py first.")
+    train_set = pd.read_csv(target_training_file)
+    val_set = pd.read_csv(target_validation_file)
+    test_set = pd.read_csv(target_test_file)
+    # Fitting tf-idf vectorizer
+    tfidf_vec = skl_text.TfidfVectorizer(min_df=0.002, max_df=0.6)
+    tfidf_vec.fit(train_set["Tweet Content"])
+
+    entenc = skl_pre.LabelBinarizer()
+    sentenc = skl_pre.LabelEncoder()
+
+    entenc.fit(train_set["Entity"].to_xarray())
+    sentenc.fit(train_set["Sentiment"])
+
+    class_names = sentenc.inverse_transform(range(4))
+    pd.DataFrame(class_names).to_csv("../data/class_names.csv")
+
+    X_train, y_train = vectorize(train_set, entenc, sentenc, tfidf_vec)
+    X_val, y_val = vectorize(val_set, entenc, sentenc, tfidf_vec)
+    X_test, y_test = vectorize(test_set, entenc, sentenc, tfidf_vec)
 
     return X_train, y_train, X_val, y_val, X_test, y_test
